01b9d14cff390090c32d75beb78cdc9c34e24df2
JoMingyu <city7310@naver.com>
2017-04-19 23:42:12 +0900

    서버 루트 디렉토리 명칭 변경
    Daejeon_People to Daejeon_People_Server

---
+++ Daejeon_People_Server/src/main/java/com/planb/restful/account/Signup.java
@@ -0,0 +1,28 @@
+package com.planb.restful.account;
+
+import com.planb.support.routing.Route;
+import com.planb.support.user.UserManager;
+
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
+
+@Route(uri = "/account/signup", method = HttpMethod.POST)
+public class Signup implements Handler<RoutingContext> {
+	UserManager userManager;
+	public Signup() {
+		userManager = new UserManager();
+	}
+	
+	@Override
+	public void handle(RoutingContext ctx) {
+		String id = ctx.request().getFormAttribute("id");
+		String email = ctx.request().getFormAttribute("email");
+		String password = ctx.request().getFormAttribute("password");
+		
+		userManager.signup(id, email, password);
+		
+		ctx.response().setStatusCode(201).end();
+		ctx.response().close();
+	}
+}
