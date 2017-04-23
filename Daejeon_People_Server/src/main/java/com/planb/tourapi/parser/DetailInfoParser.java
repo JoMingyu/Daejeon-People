d5c239cd9848d37fe1286599188821faaa8e9bb0
JoMingyu <city7310@naver.com>
2017-04-23 22:07:56 +0900

    SQLException을 방어하기 위한 로직 수정과 관광지 세부정보 파서 구현 완료

--- Daejeon_People_Server/src/main/java/com/planb/tourapi/parser/DetailInfoParser.java
+++ Daejeon_People_Server/src/main/java/com/planb/tourapi/parser/DetailInfoParser.java
@@ -2,6 +2,11 @@ package com.planb.tourapi.parser;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.json.JSONObject;
 
@@ -27,108 +32,122 @@ public class DetailInfoParser {
 		 */
 		
 		clearTables();
+		
 		ResultSet rs = database.executeQuery("SELECT * FROM attractions_basic");
+		Map<Integer, Integer> contentInfoMap = new HashMap<Integer, Integer>();
+		Set<Integer> contentIdSet = new HashSet<Integer>();
+		Iterator<Integer> contentIdIterator = null;
+		
 		try {
 			while(rs.next()) {
-				int contentId = rs.getInt("content_id");
-				int contentTypeId = rs.getInt("content_type_id");
-				JSONObject item = Request.getItem(URL + "&contentId=" + contentId + "&contentTypeId=" + contentTypeId);
-				if(contentTypeId == 12) {
-					// 12 : 관광지
-//					JSONObject item = Request.getItem(URL + "&contentId=" + contentId + "&contentTypeId=" + contentTypeId);
-					
-					String accomcount = null;
-					if(item.has("accomcount")) {
-						accomcount = item.getString("accomcount");
-					}
-					// 수용인원. 문자열 형태로, 정보가 없는 경우가 많음
-					
-					String babyCarriage = null;
-					if(item.has("chkbabycarriage")) {
-						item.getString("chkbabycarriage");
-					}
-					// 유모차 대여 여부
-					
-					String creditCard = null;
-					if(item.has("chkcreditcard")) {
-						item.getString("chkcreditcard");
-					}
-					// 신용카드 가능 여부
-					
-					String pet = null;
-					if(item.has("chkpet")) {
-						item.getString("chkpet");
-					}
-					// 애완동물 가능 여부
-					
-					String ageRange = null;
-					if(item.has("expagerange")) {
-						ageRange = item.getString("expagerange");
-					}
-					// 체험가능 연령
-					
-					String guide = item.getString("expguide");
-					// 가이드
-					
-					boolean culturalHeritage = false;
-					boolean naturalHeritage = false;
-					boolean archivalHeritage = false;
-					if(item.has("heritage1") && item.has("heritage2") && item.has("heritage3")) {
-						int heritage1 = item.getInt("heritage1");
-						int heritage2 = item.getInt("heritage2");
-						int heritage3 = item.getInt("heritage3");
-						culturalHeritage = (heritage1 == 1) ? true : false; 
-						naturalHeritage = (heritage2 == 1) ? true : false; 
-						archivalHeritage = (heritage3 == 1) ? true : false; 
-						// boolean이지만 값은 0 또는 1, 따라서 삼항으로 타입 변환
-					}
-					// 문화재 여부
-					
-					String infoCenter = null;
-					if(item.has("infocenter")) {
-						infoCenter = item.getString("infocenter");
-					}
-					// 문의처
-					
-					String openDate = null;
-					if(item.has("opendate")) {
-						openDate = item.getString("opendate");
-					}
-					// 개장일
-					
-					String restDate = null;
-					if(item.has("restdate")) {
-						restDate = item.getString("restdate");
-					}
-					// 쉬는날
-					
-					String parking = null;
-					if(item.has("parking")) {
-						parking = item.getString("parking");
-					}
-					// 주차시설
-					
-					String useSeason = null;
-					if(item.has("useseason")) {
-						useSeason = item.getString("useseason");
-					}
-					// 이용시기
-					
-					String useTime = null;
-					if(item.has("usetime")) {
-						useTime = item.getString("usetime");
-					}
-					// 이용시간
-					
-					System.out.println(item);
-				}
-				
-				database.executeUpdate("INSERT INTO tourrism_detail_info VALUES(");
-//				JSONObject item = Request.getItem(URL + "&contentId=" + contentId + "&contentTypeId=" + contentTypeId);
+				contentInfoMap.put(rs.getInt("content_id"), rs.getInt("content_type_id"));
 			}
-			System.out.println("Detail Info Parse Success.");
-		} catch (SQLException e) {
+			contentIdSet = contentInfoMap.keySet();
+			contentIdIterator = contentIdSet.iterator();
+			// To escape SQLException : Operation not allowed after ResultSet closed
+		} catch(SQLException e) {
 			e.printStackTrace();
 		}
+		
+		while (contentIdIterator.hasNext()) {
+			int contentId = contentIdIterator.next();
+			int contentTypeId = contentInfoMap.get(contentId);
+//			 JSONObject item = Request.getItem(URL + "&contentId=" + contentId + "&contentTypeId=" + contentTypeId);
+			if (contentTypeId == 12) {
+				// 12 : 관광지
+				/*
+				 * baby_carrige, credit_card, pet 모두 null
+				 * heritage 1, 2, 3 모두 0(false)
+				 */
+				JSONObject item = Request.getItem(URL + "&contentId=" + contentId + "&contentTypeId=" + contentTypeId);
+
+				String accomCount = null;
+				if (item.has("accomcount")) {
+					accomCount = item.getString("accomcount");
+				}
+				// 수용인원. 문자열 형태로, 정보가 없는 경우가 많음
+
+				String babyCarriage = null;
+				if (item.has("chkbabycarriage")) {
+					item.getString("chkbabycarriage");
+				}
+				// 유모차 대여 여부
+
+				String creditCard = null;
+				if (item.has("chkcreditcard")) {
+					item.getString("chkcreditcard");
+				}
+				// 신용카드 가능 여부
+
+				String pet = null;
+				if (item.has("chkpet")) {
+					item.getString("chkpet");
+				}
+				// 애완동물 가능 여부
+
+				String ageRange = null;
+				if (item.has("expagerange")) {
+					ageRange = item.getString("expagerange");
+				}
+				// 체험가능 연령
+
+				String guide = item.getString("expguide");
+				// 가이드
+
+				boolean culturalHeritage = false;
+				boolean naturalHeritage = false;
+				boolean archivalHeritage = false;
+				if (item.has("heritage1") && item.has("heritage2") && item.has("heritage3")) {
+					int heritage1 = item.getInt("heritage1");
+					int heritage2 = item.getInt("heritage2");
+					int heritage3 = item.getInt("heritage3");
+					culturalHeritage = (heritage1 == 1) ? true : false;
+					naturalHeritage = (heritage2 == 1) ? true : false;
+					archivalHeritage = (heritage3 == 1) ? true : false;
+					// boolean이지만 값은 0 또는 1, 따라서 삼항으로 타입 변환
+				}
+				// 문화재 여부
+
+				String infoCenter = null;
+				if (item.has("infocenter")) {
+					infoCenter = item.getString("infocenter").replace("'", "''");
+				}
+				// 문의처
+
+				String openDate = null;
+				if (item.has("opendate")) {
+					openDate = item.getString("opendate");
+				}
+				// 개장일
+
+				String restDate = null;
+				if (item.has("restdate")) {
+					restDate = item.getString("restdate");
+				}
+				// 쉬는날
+
+				String parking = null;
+				if (item.has("parking")) {
+					parking = item.getString("parking");
+				}
+				// 주차시설
+
+				String useSeason = null;
+				if (item.has("useseason")) {
+					useSeason = item.getString("useseason");
+				}
+				// 이용시기
+
+				String useTime = null;
+				if (item.has("usetime")) {
+					useTime = item.getString("usetime");
+				}
+				// 이용시간
+
+				System.out.println(item);
+				database.executeUpdate("INSERT INTO tourrism_detail_info VALUES(", contentId, ", '", accomCount, "', '", babyCarriage, "', '", creditCard, "', '", pet, "', '", ageRange, "', '", guide, "', ", culturalHeritage, ", ", naturalHeritage, ", ", archivalHeritage, ", '", infoCenter, "', '", openDate, "', '", restDate, "', '", parking, "', '", useSeason, "', '", useTime, "')");
+			}
+		}
+		System.out.println("Detail Info Parse Success.");
 	}
 }
