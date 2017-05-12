c3f704ecdf88516cf1b0a2d7e2b397b3fdc0a82d
geni429 <geni429@gmail.com>
2017-05-12 10:43:04 +0900

    phone_certified

---
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/PhoneCertified.java
@@ -0,0 +1,13 @@
+package com.example.geni.custumedittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneCertified extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.phone_certified);
+    }
+}
