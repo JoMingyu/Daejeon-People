a399d4737874b848684954f7e7cabfc2b2ff20a3
JoMingyu <city7310@naver.com>
2017-05-12 11:31:39 +0900

    간단한 테스팅

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/MainActivity.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 
 import com.daejeonpeople.R;
 import com.daejeonpeople.support.firebase.Firebase;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         startActivity(new Intent(this,Splash.class));
-        //startActivity(new Intent(this,))
     }
 }
