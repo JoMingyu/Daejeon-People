f0773b48b48aa28793a1d3f3cbcaa73b92d42769
jeong-minji <alswl4152@naver.com>
2017-05-16 14:32:04 +0900

    first introduction

---
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Email_Certified.java
@@ -0,0 +1,15 @@
+package com.daejeonpeople.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daejeonpeople.R;
+
+public class Email_Certified extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.email__certified);
+    }
+}
---
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Introduction_CulturalFacilities.java
@@ -0,0 +1,15 @@
+package com.daejeonpeople.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daejeonpeople.R;
+
+public class Introduction_CulturalFacilities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction__cultural_facilities);
+    }
+}
