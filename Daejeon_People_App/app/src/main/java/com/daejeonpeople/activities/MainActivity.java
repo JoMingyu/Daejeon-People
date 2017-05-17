53071531257457ee396ec5e8e2a5f5f0dff8151b
geni429 <geni429@gmail.com>
2017-05-17 23:16:30 +0900

    chatting -> MessageBox구현

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/MainActivity.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.invitation_trip_listview);
+        setContentView(R.layout.chatting);
         startActivity(new Intent(this,Splash.class));
     }
 }
