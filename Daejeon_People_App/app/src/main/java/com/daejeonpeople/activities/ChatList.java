de983f682821a79a485904800ac7b97d494b3ccc
jeong-minji <alswl4152@naver.com>
2017-05-19 13:16:20 +0900

    chatlist listview

---
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/ChatList.java
@@ -0,0 +1,15 @@
+package com.daejeonpeople.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daejeonpeople.R;
+
+public class ChatList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_list_listview);
+    }
+}
