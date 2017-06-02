e05e0f986607069cb05b7be7cf730edc7baf5650
JoMingyu <city7310@naver.com>
2017-06-02 22:34:39 +0900

    문서화 어노테이션

--- Daejeon_People_Server/src/main/java/com/planb/restful/travel/inside/GetUnreadMessages.java
+++ Daejeon_People_Server/src/main/java/com/planb/restful/travel/inside/GetUnreadMessages.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 import com.planb.support.chatting.ChatManager;
 import com.planb.support.chatting.MySQL_Chat;
 import com.planb.support.routing.API;
+import com.planb.support.routing.REST;
 import com.planb.support.routing.Route;
 
 import io.vertx.core.Handler;
@@ -16,6 +17,7 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
 @API(functionCategory = "여행 모드 내부", summary = "읽지 않은 채팅 기록 조회")
+@REST(requestBody = "topic : String, idx : int", responseBody = "idx : int, type : String, name : String, content : String, (JSONArray)", successCode = 200, failureCode = 204)
 @Route(uri = "/chat/read", method = HttpMethod.POST)
 public class GetUnreadMessages implements Handler<RoutingContext> {
 	@Override
