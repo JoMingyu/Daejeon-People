55d54e62a6fd625f2b98b7ac9cb419442f00f12b
JoMingyu <city7310@naver.com>
2017-06-04 15:40:17 +0900

    Firebase 클래스 개선

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/support/firebase/Firebase.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/support/firebase/Firebase.java
@@ -4,15 +4,22 @@ import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 /**
- * Created by dsm2016 on 2017-05-11.
+ * Created by JoMingyu on 2017-05-11.
  */
 
 public class Firebase extends FirebaseInstanceIdService {
     public static String getFirebaseToken() {
-        String token = FirebaseInstanceId.getInstance().getToken();
+        return FirebaseInstanceId.getInstance().getToken();
+    }
 
-        return token;
+    public static void subscribeTopic(String topicName) {
+        FirebaseMessaging.getInstance().subscribeToTopic(topicName);
+    }
+
+    public static void unsubscribeTopic(String topicName) {
+        FirebaseMessaging.getInstance().subscribeToTopic(topicName);
     }
 }
