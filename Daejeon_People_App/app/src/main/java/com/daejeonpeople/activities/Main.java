ed955dcac586420d5ef4d47725df1081199082a4
PpalPpali <kdgbang@gmail.com>
2017-06-20 19:59:00 +0900

    bottom menu
    I Create bottom menu~

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Main.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Main.java
@@ -1,20 +1,32 @@
 package com.daejeonpeople.activities;
 
+import android.app.TabActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 import com.daejeonpeople.R;
 
 //동규
 
-public class Main extends Activity {
+public class Main extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.filter);
+        setContentView(R.layout.main);
         startActivity(new Intent(this,Splash.class));
 
+        TabHost tabHost = getTabHost();
+
+        TabSpec tabSpecTab1 = tabHost.newTabSpec("TAB1").setIndicator("메인으로");
+        tabSpecTab1.setContent(R.id.tab1);
+        tabHost.addTab(tabSpecTab1);
+
+        TabSpec tabSpecTab2 = tabHost.newTabSpec("TAB2").setIndicator("카테고리");
+        tabSpecTab2.setContent(R.id.tab2);
+        tabHost.addTab(tabSpecTab2);
     }
 }
