c5f82a3609b3b3e12522d82e46a4d1e9ce44f193
JoMingyu <city7310@naver.com>
2017-07-03 22:34:08 +0900

    이메일 EditText 텍스트 변경 리스너, 발급과 인증 여부 플래그와 스낵바 적용 등

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Email_Certified.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Email_Certified.java
@@ -4,7 +4,10 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,23 +35,42 @@ public class Email_Certified extends AppCompatActivity {
     private Button confirmButton;
 
     private int statusCode;
-    public static boolean emailDemanded = false;
+    private boolean emailDemanded = false;
+    public static boolean emailCertified = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.email_certified);
 
-        confirmButton = (Button)findViewById(R.id.confirmBtn);
-        email = (EditText)findViewById(R.id.userEmail);
+        confirmButton = (Button) findViewById(R.id.confirmBtn);
+        email = (EditText) findViewById(R.id.email);
+        checkCode = (EditText) findViewById (R.id.checkCode);
+
         emailDemanded = false;
         // 액티비티 로드마다 false
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                emailDemanded = false;
+                confirmButton.setText("발급");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
 
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 if(!emailDemanded){
+                    // 버튼이 눌렸을 때 이메일 인증번호가 전송되지 않은 상태라면
+
                     params.put("email", email.getText().toString());
                     aQuery = new AQuery(getApplicationContext());
                     aQuery.ajax("http://52.79.134.200/signup/email/demand", params, String.class, new AjaxCallback<String>(){
@@ -56,26 +78,37 @@ public class Email_Certified extends AppCompatActivity {
                         public void callback(String url, String response, AjaxStatus status) {
                             statusCode = status.getCode();
                             if(statusCode == 201){
+                                emailDemanded = true;
+                                // 이메일 인증 번호가 전송되었음을 표시
+
+                                email.setTextColor(Color.rgb(111, 186, 119));
+                                // 이메일 텍스트 컬러를 바꿈
+
                                 confirmButton.setText("인증");
                                 ShowDialog();
                             } else {
-                                Toast.makeText(getApplicationContext(), "이미 존재하는 이메일입니다.", Toast.LENGTH_SHORT).show();
+                                emailDemanded = false;
+                                email.setTextColor(Color.rgb(252, 113, 80));
+                                Snackbar.make(getWindow().getDecorView().getRootView(), "이미 존재하는 이메일입니다.", 1000).show();
                             }
                         }
                     });
-                } else if(emailDemanded){
-                    checkCode = (EditText)findViewById(R.id.checkCode);
+                } else {
+                    // 전송된 상태라면
                     params.put("code",checkCode.getText().toString());
                     aQuery.ajax("http://52.79.134.200/signup/email/verify", params, String.class, new AjaxCallback<String>(){
                        @Override
                         public void callback(String url, String response, AjaxStatus status){
                             if(status.getCode() == 201){
-                                emailDemanded = true;
+                                emailCertified = true;
+                                // 인증 완료되었음을 표시
+
                                 Intent intent = new Intent(getApplicationContext(), SignUp.class);
                                 intent.putExtra("email", email.getText().toString());
                                 startActivity(intent);
                             } else {
-                                Toast.makeText(getApplicationContext(), "인증번호가 맞지 않습니다.", Toast.LENGTH_SHORT).show();
+                                emailCertified = false;
+                                Snackbar.make(getWindow().getDecorView().getRootView(), "인증번호가 맞지 않습니다.", 1000).show();
                             }
                        }
                     });
