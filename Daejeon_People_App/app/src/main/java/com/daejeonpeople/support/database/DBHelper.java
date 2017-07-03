30a0b51e610ef11538c407bdc1887f5217127e57
geni429 <geni429@gmail.com>
2017-07-03 15:46:12 +0900

    DBHelper 추가

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/support/database/DBHelper.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/support/database/DBHelper.java
@@ -1,7 +1,6 @@
 package com.daejeonpeople.support.database;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
