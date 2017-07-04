27ae3ee4a72fbed613dbb1e2dd712995537dff98
PpalPpali <kdgbang@gmail.com>
2017-07-04 18:59:37 +0900

    I commit!!!!!
    I commit!!!

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Main.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Main.java
@@ -2,10 +2,7 @@ package com.daejeonpeople.activities;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
 
 import com.daejeonpeople.R;
