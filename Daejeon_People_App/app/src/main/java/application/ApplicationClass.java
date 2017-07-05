49d45bbe2f792fccb2469f0cebd218ddd00471c8
JoMingyu <city7310@naver.com>
2017-07-05 10:28:29 +0900

    ApplicationClass와 폰트 적용

---
+++ Daejeon_People_App/app/src/main/java/application/ApplicationClass.java
@@ -0,0 +1,17 @@
+package application;
+
+import android.app.Application;
+
+import com.tsengvn.typekit.Typekit;
+
+/**
+ * Created by JoMingyu on 2017-07-05.
+ */
+
+public class ApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Typekit.getInstance().addNormal(Typekit.createFromAsset(this, "NanumSquareL.ttf"));
+    }
+}
