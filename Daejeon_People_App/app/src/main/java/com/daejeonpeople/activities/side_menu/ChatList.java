959ace6c55e57954c47ced736267bef0709dec54
JoMingyu <city7310@naver.com>
2017-07-20 22:30:30 +0900

    chatlist, friendlist, wishlist 뒤로가기

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/side_menu/ChatList.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/side_menu/ChatList.java
@@ -1,10 +1,13 @@
 package com.daejeonpeople.activities.side_menu;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.daejeonpeople.R;
+import com.daejeonpeople.activities.Main;
 import com.daejeonpeople.activities.base.BaseActivity;
 //민지
 
@@ -29,5 +32,12 @@ public class ChatList extends BaseActivity {
         chatting.setDisplayShowHomeEnabled(false);
 
         backBtn = (Button) findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Main.class));
+            }
+        });
     }
 }
--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/side_menu/WishList.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/side_menu/WishList.java
@@ -2,10 +2,13 @@ package com.daejeonpeople.activities.side_menu;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.daejeonpeople.R;
+import com.daejeonpeople.activities.Main;
 import com.daejeonpeople.activities.base.BaseActivity;
 
 /**
@@ -33,5 +36,12 @@ public class WishList extends BaseActivity {
         chatting.setDisplayShowHomeEnabled(false);
 
         backBtn = (Button) findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Main.class));
+            }
+        });
     }
 }
