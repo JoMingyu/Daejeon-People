fc1932a38313f43741372ef965b81527fef3bb58
geni429 <geni429@gmail.com>
2017-07-31 00:39:56 +0900

    Login, Sign_up 앞글자가 대문자여서 생기던 이슈 소문자로 바꿔서 해결 및 find_id시에 찾은 id를 조회할 수 있는 dialog추가. dialog에서 요청처리하는지 몰라서 간략한 요청코드만 추가해놓음

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/account/EntryID.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/account/EntryID.java
@@ -1,10 +1,29 @@
 package com.daejeonpeople.activities.account;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
 import com.daejeonpeople.R;
 import com.daejeonpeople.activities.base.BaseActivity;
+import com.daejeonpeople.support.views.ColorManager;
+import com.daejeonpeople.support.views.SnackbarManager;
+import com.daejeonpeople.valueobject.UserInSignup;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by geni on 2017. 5. 28..
@@ -12,10 +31,31 @@ import com.daejeonpeople.activities.base.BaseActivity;
 
 //민지
 
-public class EntryID extends BaseActivity {
+public class EntryID extends AppCompatActivity {
+    AQuery aQuery;
+    Button okBtn;
+    EditText inputCode;
+    Map<String, String> params;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_find_id_input);
+        okBtn = (Button)findViewById(R.id.okBtn);
+        inputCode = (EditText)findViewById(R.id.input_code);
+        aQuery = new AQuery(getApplicationContext());
+        params = new HashMap<>();
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aQuery.ajax("http://52.79.134.200/signin", params, String.class, new AjaxCallback<String>(){
+                    @Override
+                    public void callback(String url, String object, AjaxStatus status) {
+
+                    }
+                });
+            }
+        });
     }
 }
--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/account/FindID.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/account/FindID.java
@@ -5,10 +5,12 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.androidquery.AQuery;
 import com.androidquery.callback.AjaxCallback;
@@ -105,8 +107,8 @@ public class FindID extends BaseActivity {
 
                                 try {
                                     JSONObject resp = new JSONObject(response);
-
                                     String id = AES.decrypt(resp.getString("id"));
+                                    ShowIdDialog(id);
                                 } catch(JSONException e) {
                                     e.printStackTrace();
                                 }
@@ -116,6 +118,7 @@ public class FindID extends BaseActivity {
                         }
                     });
                     //전송된 id 확인할 레이아웃 제작 필요 - 민지
+                    //근철이 제작했다. - 민지
                 }
             }
         });
@@ -152,4 +155,82 @@ public class FindID extends BaseActivity {
             }
         });
     }
+
+    //성공시 찾은 ID를 보여주는 Dialog
+    private void ShowIdDialog(String id){
+        LayoutInflater dialog = LayoutInflater.from(this);
+        final View dialogLayout = dialog.inflate(R.layout.dialog_find_id_certified, null);
+        final Dialog mShowIdDialog = new Dialog(this);
+
+        mShowIdDialog.setTitle("아이디 찾기 성공");
+        mShowIdDialog.setContentView(dialogLayout);
+        mShowIdDialog.show();
+
+        EditText findIdResult = (EditText)dialogLayout.findViewById(R.id.findIdResult);
+        Button okBtn = (Button)dialogLayout.findViewById(R.id.okBtn);
+        Button cancelBtn = (Button)dialogLayout.findViewById(R.id.cancelBtn);
+
+        findIdResult.setEnabled(false);
+        findIdResult.setText(id);
+
+        okBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mShowIdDialog.cancel();
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mShowIdDialog.cancel();
+            }
+        });
+    }
+
+    //Dialog내에서 요청처리할건가요?
+
+//    private void ShowDialog()
+//    {
+//        LayoutInflater dialog = LayoutInflater.from(this);
+//        final View dialogLayout = dialog.inflate(R.layout.dialog_find_id_input, null);
+//        final Dialog myDialog = new Dialog(this);
+//
+//        Button okBtn = (Button)dialogLayout.findViewById(R.id.okBtn);
+//        final EditText inputCode = (EditText)dialogLayout.findViewById(R.id.input_code);
+//        Button cancelBtn = (Button)dialogLayout.findViewById(R.id.cancelBtn);
+//
+//        myDialog.setTitle("인증코드 입력");
+//        myDialog.setContentView(dialogLayout);
+//        myDialog.show();
+//
+//        okBtn.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                params.put("email", inputEmail.getText().toString());
+//                params.put("code", inputCode.getText().toString());
+//                aQuery.ajax("http://52.79.134.200/find/id/verify", params, String.class, new AjaxCallback<String>(){
+//                    @Override
+//                    public void callback(String url, String object, AjaxStatus status) {
+//                        Log.d("Find id Status Code", status.getCode()+"");
+//                    }
+//                });
+//            }
+//        });
+//
+//        cancelBtn.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                myDialog.cancel();
+//            }
+//        });
+//    }
 }
