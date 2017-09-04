49e097d20379438bbcc418f87f330e430ee212e3
PlanB <city7310@naver.com>
2017-09-04 11:30:02 +0900

    MyPage의 문제 해결
    로그인이 돼있는걸 부정하는 오류

--- Daejeon_People_Server/src/main/java/com/planb/restful/user/common/MyPage.java
+++ Daejeon_People_Server/src/main/java/com/planb/restful/user/common/MyPage.java
@@ -12,17 +12,11 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
 @API(functionCategory = "사용자", summary = "마이페이지")
-@REST(responseBody = "email : String, phone_number : String, name : String", successCode = 200, failureCode = 204)
+@REST(responseBody = "email : String, phone_number : String, name : String", successCode = 200)
 @Route(uri = "/mypage", method = HttpMethod.GET)
 public class MyPage implements Handler<RoutingContext> {
 	@Override
 	public void handle(RoutingContext ctx) {
-		if(UserManager.isLogined(ctx)) {
-			ctx.response().setStatusCode(204).end();
-			ctx.response().close();
-			return;
-		}
-
 		String clientId = UserManager.getEncryptedIdFromSession(ctx);
 		
 		JSONObject response = UserManager.getUserInfo(clientId);
