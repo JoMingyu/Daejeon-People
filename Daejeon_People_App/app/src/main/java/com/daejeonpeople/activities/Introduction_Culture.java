4fdf0f05357fe84191cb2a3e35856612b8b00615
Jeong-minji <alswl4152@naver.com>
2017-10-02 15:17:19 +0900

    friendlist_recycler

--- Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Introduction_Culture.java
+++ Daejeon_People_App/app/src/main/java/com/daejeonpeople/activities/Introduction_Culture.java
@@ -1,77 +1,62 @@
 package com.daejeonpeople.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
 
 import com.daejeonpeople.R;
 import com.daejeonpeople.activities.base.BaseActivity;
-import com.daejeonpeople.adapter.CulturalAdapter;
 import com.daejeonpeople.adapter.WishlistAdapter;
+import com.daejeonpeople.support.converter.CategoryConverter;
 import com.daejeonpeople.support.database.DBHelper;
 import com.daejeonpeople.support.network.APIClient;
 import com.daejeonpeople.support.network.APIinterface;
 import com.daejeonpeople.valueobject.CulturalItem;
 import com.daejeonpeople.valueobject.WishlistItem;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Jeong Minji on 2017-07-03.
  */
 
-public class Introduction_Culture extends BaseActivity {
-//
-//    private ImageButton btn_star;
-//    private APIinterface apIinterface;
-//    private ArrayList<CulturalItem> Dataset;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.introduction_cultural);
-//
-//        DBHelper dbHelper = DBHelper.getInstance(getApplicationContext(), "CHECK.db", null, 1);
-//
-//        btn_star = (ImageButton)findViewById(R.id.star);
-//        apIinterface = APIClient.getClient().create(APIinterface.class);
-//
-//        apIinterface.getDetail("UserSession="+dbHelper.getCookie()).enqueue(new Callback<JsonObject>() {
-//            @Override
-//            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-//                if(response.code()== 200) {
-//                    Log.d("response", "SUCCESS");
-//                    Gson gson = new Gson();
-//                    CulturalItem[] items = gson.fromJson(response.body().toString(), CulturalItem[].class);
-////                    ((CulturalAdapter))
-////                    ((CulturalAdapter)mRecyclerView.getAdapter()).setData(items);
-////                    mRecyclerView.getAdapter().notifyDataSetChanged();
-//
-//                    response.body();
-//                } else if(response.code() == 204) {
-//                    Log.d("response", "FAIL");
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<JsonObject> call, Throwable t) {
-//                t.printStackTrace();
-//            }
-//        });
-//
-//        btn_star.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-//
-//    }
+public class Introduction_Culture extends AppCompatActivity {
+
+    private ImageButton btn_star;
+    private APIinterface apIinterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction_cultural);
+
+        DBHelper dbHelper = DBHelper.getInstance(getApplicationContext(), "CHECK.db", null, 1);
+
+        btn_star = (ImageButton)findViewById(R.id.star);
+        apIinterface = APIClient.getClient().create(APIinterface.class);
+
+        apIinterface.getDetail(25).enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                Log.d("Response", response.body() + "");
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+
+            }
+        });
+
+    }
 }
