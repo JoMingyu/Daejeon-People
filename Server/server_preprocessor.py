289ab7d50ef1f64135edcb947addfeb1d5af6918
JoMingyu <city7310@naver.com>
2017-10-09 02:31:48 +0900

    [ADD] server_preprocessor.py

---
+++ Server/server_preprocessor.py
@@ -0,0 +1,66 @@
+from flask import Flask, current_app
+from flask_restful_swagger_2 import Api
+
+
+def decorate(app):
+    """
+    Decorate request contexts to logging
+
+    :param app: flask.Flask instance
+
+    :type app: Flask
+
+    :rtype: None
+    """
+    @app.before_first_request
+    def before_first_request():
+        def make_logger():
+            from logging.handlers import RotatingFileHandler
+            from logging import Formatter, INFO
+
+            handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
+            handler.setFormatter(Formatter("[%(asctime)s] %(levelname)s - %(message)s"))
+
+            current_app.logger.addHandler(handler)
+            current_app.logger.setLevel(INFO)
+
+        make_logger()
+        current_app.logger.info('------ Logger Initialized ------')
+
+    @app.before_request
+    def before_request():
+        from flask import request
+
+        current_app.logger.info('Requested from {0} [ {1} {2} ]'.format(request.host, request.method, request.url))
+        current_app.logger.info('Request values : {0}'.format(request.values))
+
+    @app.after_request
+    def after_request(response):
+        current_app.logger.info('Respond : {0}'.format(response.status))
+
+        return response
+
+    @app.teardown_request
+    def teardown_request(exception):
+        if not exception:
+            current_app.logger.info('Teardown request successfully.')
+
+    @app.teardown_appcontext
+    def teardown_appcontext(exception):
+        if not exception:
+            current_app.logger.info('Teardown appcontext successfully.')
+
+
+def add_resources(app, api_version):
+    """
+    Add resources to Flask instance
+
+    :param app: flask.Flask instance
+    :param api_version: Api version
+
+    :type app: Flask
+    :type api_version: float
+
+    :rtype: None
+    """
+    api = Api(app, api_version=api_version)
