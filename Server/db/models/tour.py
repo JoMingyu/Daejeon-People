86b4c488b86f44b749dde9458bb02f8d43556105
PlanB <city7310@naver.com>
2017-10-10 22:10:11 +0900

    [UPDATE] Change field types, add insert methods

--- Server/db/models/tour.py
+++ Server/db/models/tour.py
@@ -2,8 +2,8 @@ from db.mongo import *
 
 
 class TourModel(Document):
-    content_id = StringField(primary_key=True)
-    content_type_id = StringField(required=True)
+    content_id = IntField(primary_key=True)
+    content_type_id = IntField(required=True)
     title = StringField()
     address = StringField()
     x = FloatField()
@@ -12,7 +12,7 @@ class TourModel(Document):
     main_category = StringField()
     middle_category = StringField()
     small_category = StringField()
-    tel_name = StringField()
+    tel_owner = StringField()
     tel = StringField()
     img_big_url = StringField()
     img_small_url = StringField()
@@ -35,13 +35,42 @@ class TourBase(TourModel):
 
 class TouristAttractionModel(TourBase):
     # content type id 12
-    pass
+    @classmethod
+    def insert(cls, tour, detail_images):
+        cls(
+            content_id=tour['content_id'],
+            content_type_id=tour['content_type_id'],
+            title=tour['title'],
+            address=tour['address'],
+            x=tour['x'],
+            y=tour['y'],
+            main_category=tour['main_category'],
+            middle_category=tour['middle_category'],
+            small_category=tour['small_category'],
+            tel_owner=tour['tel_owner'],
+            tel=tour['tel'],
+            img_big_url=tour['img_big_url'],
+            img_small_url=tour['img_small_url'],
+            views=tour['views'],
+            images=detail_images,
+            credit_card=tour['credit_card'],
+            baby_carriage=tour['baby_carriage'],
+            pet=tour['pet'],
+            info_center=tour['info_center'],
+            use_time=tour['use_time'],
+            rest_date=tour['rest_date']).save()
 
 
 class CulturalFacilityModel(TourBase):
     # content type id 14
     use_fee = StringField()
     spend_time = StringField()
+
+    @classmethod
+    def insert(cls, tour, detail_images):
+        cls(
+
+        ).save()
 
 
 class FestivalModel(TourModel):
@@ -52,16 +81,34 @@ class FestivalModel(TourModel):
     spend_time = StringField()
     place = StringField()
 
+    @classmethod
+    def insert(cls, tour, detail_images):
+        cls(
+
+        ).save()
+
 
 class TourCourseModel(TourModel):
     # content type id 25
     spend_time = StringField()
     distance = StringField()
 
+    @classmethod
+    def insert(cls, tour, detail_images):
+        cls(
+
+        ).save()
+
 
 class LeisureModel(TourBase):
     # content type id 28
     use_fee = StringField()
+
+    @classmethod
+    def insert(cls, tour, detail_images):
+        cls(
+
+        ).save()
 
 
 class AccommodationModel(TourModel):
@@ -73,10 +120,20 @@ class AccommodationModel(TourModel):
     goodstay = BooleanField()
     capacity = StringField()
 
+    @classmethod
+    def insert(cls, tour, detail_images):
+        cls(
+
+        ).save()
+
 
 class ShoppingModel(TourBase):
     # content type id 38
-    pass
+    @classmethod
+    def insert(cls, tour, detail_images):
+        cls(
+
+        ).save()
 
 
 class RestaurantModel(TourModel):
@@ -86,3 +143,9 @@ class RestaurantModel(TourModel):
     use_time = StringField()
     rest_date = StringField()
     rep_menu = StringField()
+
+    @classmethod
+    def insert(cls, tour, detail_images):
+        cls(
+
+        ).save()
