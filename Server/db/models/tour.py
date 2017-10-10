0e6f4fe15862406d950f1ad6bd48c798a0f08605
PlanB <city7310@naver.com>
2017-10-10 15:06:12 +0900

    [DONE] TourBase, Detail Models

--- Server/db/models/tour.py
+++ Server/db/models/tour.py
@@ -26,3 +26,67 @@ class TourModel(Document):
     images = ListField(StringField())
 
     meta = {'allow_inheritance': True}
+
+
+class TourBase(TourModel):
+    credit_card = StringField()
+    baby_carriage = StringField()
+    pet = StringField()
+    info_center = StringField()
+    use_time = StringField()
+    rest_date = StringField()
+
+
+class TouristAttractionModel(TourBase):
+    # content type id 12
+    pass
+
+
+class CulturalFacilityModel(TourBase):
+    # content type id 14
+    use_fee = StringField()
+    spend_time = StringField()
+
+
+class FestivalModel(TourModel):
+    # content type id 15
+    start_date = StringField()
+    end_date = StringField()
+    use_fee = StringField()
+    spend_time = StringField()
+    place = StringField()
+
+
+class TourCourseModel(TourModel):
+    # content type id 25
+    spend_time = StringField()
+    distance = StringField()
+
+
+class LeisureModel(TourBase):
+    # content type id 28
+    use_fee = StringField()
+
+
+class AccommodationModel(TourModel):
+    # content type id 32
+    info_center = StringField()
+    checkin_time = StringField()
+    checkout_time = StringField()
+    benikia = BooleanField()
+    goodstay = BooleanField()
+    capacity = StringField()
+
+
+class ShoppingModel(TourBase):
+    # content type id 38
+    pass
+
+
+class RestaurantModel(TourModel):
+    # content type id 39
+    credit_card = StringField()
+    info_center = StringField()
+    use_time = StringField()
+    rest_date = StringField()
+    rep_menu = StringField()
