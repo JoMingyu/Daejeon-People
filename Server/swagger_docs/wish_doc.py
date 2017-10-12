6be92bf4e19a43eb29362dcf0318d11528454a63
PlanB <city7310@naver.com>
2017-10-12 14:10:28 +0900

    [DONE] WishList API Swagger

---
+++ Server/swagger_docs/wish_doc.py
@@ -0,0 +1,54 @@
+WISH_ADD = {
+    'tags': ['위시리스트'],
+    'description': '위시리스트에 여행지 추가(JWT Required)',
+    'parameters': [
+        {
+            'name': 'content_id',
+            'description': '위시리스트에 추가할 여행지의 content id',
+            'in': 'formData',
+            'type': 'int'
+        }
+    ],
+    'responses': {
+        '201': {
+            'description': '위시리스트 등록 성공'
+        },
+        '204': {
+            'description': 'content id에 해당하는 여행지가 존재하지 않거나, 이미 위시리스트에 등록되어 있음'
+        }
+    }
+}
+
+WISH_GET = {
+    'tags': ['위시리스트'],
+    'description': '위시리스트 목록 조회(JWT Required)',
+    'responses': {
+        '200': {
+            'description': '위시리스트 목록 조회 성공',
+            'examples': {
+                'application/json': [
+                    250577,
+                    1622646
+                ]
+            }
+        }
+    }
+}
+
+WISH_DELETE = {
+    'tags': ['위시리스트'],
+    'description': '위시리스트에서 여행지 제거(JWT Required)',
+    'parameters': [
+        {
+            'name': 'content_id',
+            'description': '위시리스트에서 제거할 content id',
+            'in': 'formData',
+            'type': 'int'
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '삭제 성공'
+        }
+    }
+}
